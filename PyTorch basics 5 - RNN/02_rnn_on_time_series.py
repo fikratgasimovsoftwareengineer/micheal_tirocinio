252366e38e67de04ba896825269f317390a67b84
Mikkun1551 <michelcalvaresi1551@gmail.com>
2024-05-09 16:49:39 +0200

    Aggiornamento RNN

--- PyTorch basics 5 - RNN/02_rnn_on_time_series.py
+++ PyTorch basics 5 - RNN/02_rnn_on_time_series.py
@@ -1,3 +1,4 @@
+import time
 import torch
 import torch.nn as nn
 import numpy as np
@@ -17,8 +18,10 @@ print(len(df))
 # Devi decidere cosa fare se il tuo dataframe ha dei valori nulli
 # in questo caso non li ha
 df = df.dropna()
+"""
 df.plot(figsize=(12, 4))
 plt.show()
+"""
 # Il df tratta i numeri come object, rendiamoli float
 y = df['S4248SM144NCEN'].values.astype(float)
 # Ora prendiamo solo gli ultimi 12 mesi per i test data
@@ -55,4 +58,105 @@ def input_data(seq, ws):
 train_data = input_data(train_norm, window_size)
 
 
+class LSTMnetwork(nn.Module):
+    def __init__(self, input_size=1, hidden_size=100, output_size=1):
+        super().__init__()
+        self.hidden_size = hidden_size
+        # Add an LSTM layer:
+        self.lstm = nn.LSTM(input_size, hidden_size)
+        # Add a fully-connected layer:
+        self.linear = nn.Linear(hidden_size, output_size)
+        # Initialize h0 and c0:
+        self.hidden = (torch.zeros(1, 1, self.hidden_size),
+                       torch.zeros(1, 1, self.hidden_size))
 
+    def forward(self, seq):
+        lstm_out, self.hidden = self.lstm(
+            seq.view(len(seq), 1, -1), self.hidden)
+        pred = self.linear(lstm_out.view(len(seq), -1))
+        return pred[-1]  # we only want the last value
+
+torch.manual_seed(101)
+model = LSTMnetwork()
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+epoch = 100
+start_time = time.time()
+for i in range(epoch):
+    for seq, y_train in train_data:
+        optimizer.zero_grad()
+        model.hidden = (torch.zeros(1, 1, model.hidden_size),
+                        torch.zeros(1, 1, model.hidden_size))
+        y_pred = model(seq)
+        loss = criterion(y_pred, y_train)
+        loss.backward()
+        optimizer.step()
+    print(f'Epoch {i+1} Loss {loss.item()}')
+total_time = time.time() - start_time
+print(f'Training took {total_time/60} minutes')
+future = 12
+preds = train_norm[-window_size:].tolist()
+model.eval()
+for i in range(future):
+    seq = torch.FloatTensor(preds[-window_size:])
+    with torch.no_grad():
+        model.hidden = (torch.zeros(1, 1, model.hidden_size),
+                        torch.zeros(1, 1, model.hidden_size))
+        preds.append(model(seq).item())
+# Ora devo invertire la normalizzazione
+true_pred = scaler.inverse_transform(np.array(preds[window_size:]).reshape(-1, 1))
+print(true_pred)
+print(df['S4248SM144NCEN'][-12:])
+# Mettiamo a grafico
+x = np.arange('2018-02-01', '2019-02-01', dtype='datetime64[M]')
+plt.figure(figsize=(12,4))
+plt.title('Beer, Wine, and Alcohol Sales')
+plt.ylabel('Sales (millions of dollars)')
+plt.grid(True)
+plt.autoscale(axis='x',tight=True)
+plt.plot(df['S4248SM144NCEN'])
+plt.plot(x,true_pred)
+plt.show()
+epochs = 100
+# set model to back to training mode
+model.train()
+# feature scale the entire dataset
+y_norm = scaler.fit_transform(y.reshape(-1, 1))
+y_norm = torch.FloatTensor(y_norm).view(-1)
+all_data = input_data(y_norm, window_size)
+start_time = time.time()
+for epoch in range(epochs):
+    # train on the full set of sequences
+    for seq, y_train in all_data:
+        # reset the parameters and hidden states
+        optimizer.zero_grad()
+        model.hidden = (torch.zeros(1, 1, model.hidden_size),
+                        torch.zeros(1, 1, model.hidden_size))
+        y_pred = model(seq)
+        loss = criterion(y_pred, y_train)
+        loss.backward()
+        optimizer.step()
+    # print training result
+    print(f'Epoch: {epoch + 1:2} Loss: {loss.item():10.8f}')
+print(f'\nDuration: {time.time() - start_time:.0f} seconds')
+window_size = 12
+future = 12
+L = len(y)
+preds = y_norm[-window_size:].tolist()
+model.eval()
+for i in range(future):
+    seq = torch.FloatTensor(preds[-window_size:])
+    with torch.no_grad():
+        model.hidden = (torch.zeros(1, 1, model.hidden_size),
+                        torch.zeros(1, 1, model.hidden_size))
+        preds.append(model(seq).item())
+true_pred = scaler.inverse_transform(np.array(preds).reshape(-1, 1))
+x = np.arange('2019-02-01', '2020-02-01', dtype='datetime64[M]').astype('datetime64[D]')
+plt.figure(figsize=(12,4))
+plt.title('Beer, Wine, and Alcohol Sales')
+plt.ylabel('Sales (millions of dollars)')
+plt.grid(True)
+plt.autoscale(axis='x',tight=True)
+plt.plot(df['S4248SM144NCEN'])
+plt.plot(x, true_pred[window_size:])
+plt.show()
